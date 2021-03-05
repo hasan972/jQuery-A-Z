56857b3c03267155acfc057f4fad3ff58d7766d7
Rakibul Hasan <16201100@uap-bd.edu>
2021-03-06 01:05:12 +0600

    CSS style add code for different style

--- app.js
+++ app.js
@@ -23,5 +23,9 @@ $(document.getElementById("san1")).ready(function(){
 //$("a").attr("href");
 // $("a").setAttr("href","https://github.com/hasan972");
   
+// image manipulation..
 
-
+// simple rule >> $("h3").css("color","red");
+// jason syyle >> $("h3").css({"color":"red", "font-size":"2rem"});
+//for javaScript >> document.getElement("h3").classList.add/remove("style1");
+// External css add >> $("h3").addClass("style1");
