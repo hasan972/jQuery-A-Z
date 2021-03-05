b91541dede2d3bbe58b89fe96a8afc2b06efd2fe
Rakibul Hasan <16201100@uap-bd.edu>
2021-03-05 23:25:16 +0600

    First jQuery code in this repository

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document.getElementById("san1")).ready(function(){
+    $("button").click(function(){
+      $("p").html("Hello <b>world!</b>");
+    });
+  });
+  
+  $(document.getElementById("san2")).ready(function(){
+      $("button").click(function(){
+        $("h1").html("change");
+
+      });
+  });
+
